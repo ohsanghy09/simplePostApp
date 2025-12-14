2b15ea3cf3d904879096f8e5af75ac2c08e1025a
user <ohsanghy09@naver.com>
2025-12-14 16:37:38 +0900

    전체 인덱스 주석

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return(
+
+    // 화면 전환, 기본화면으로는 pathname으로 설정
   <Redirect href={{
     pathname : "/(tabs)/home"
       }} 
